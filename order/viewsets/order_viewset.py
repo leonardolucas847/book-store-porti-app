cfb3599a68d8fccb71e4de1b359be44c43b17178
Leonardo Lucas monteiro costa <leonardolucas847@gmail.com>
2026-09-14 14:56:56 -0300

    retirado aautenticaçao de order

--- order/viewsets/order_viewset.py
+++ order/viewsets/order_viewset.py
@@ -1,34 +1,8 @@
-from rest_framework.authentication import TokenAuthentication, SessionAuthentication, BasicAuthentication
-from rest_framework.permissions import IsAuthenticated
 from rest_framework.viewsets import ModelViewSet
-from rest_framework.authentication import SessionAuthentication, BasicAuthentication, TokenAuthentication
-from rest_framework.permissions import IsAuthenticated
 from order.models import Order
 from order.serializers import OrderSerializer
 
 
 class OrderViewSet(ModelViewSet):
-    authentication_classes = [SessionAuthentication, BasicAuthentication,TokenAuthentication]
-    permission_classes = [IsAuthenticated]
-
-
     serializer_class = OrderSerializer
     queryset = Order.objects.all().order_by("id")
-
-
-from django.contrib.auth.models import User
-from rest_framework.test import APITestCase
-
-
-class TestOrderViewSet(APITestCase):
-    def setUp(self):
-        # 1. Cria o usuário de teste
-        self.user = User.objects.create_user(
-            username="testuser",
-            password="password123"
-        )
-
-        # 2. Força a autenticação do cliente para passar no IsAuthenticated
-        self.client.force_authenticate(user=self.user)
-
-        # ... Mantenha o restante do seu setUp (criação de produtos, categorias, etc.)
